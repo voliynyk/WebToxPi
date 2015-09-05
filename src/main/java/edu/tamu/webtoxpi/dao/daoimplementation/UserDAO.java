e5a8bdc5c03c2109b51aed283e7f0ca1fe231f49
Viktor <pascal902@gmail.com>
2015-09-05 12:32:08 -0500

    It works

---
+++ src/main/java/edu/tamu/webtoxpi/dao/daoimplementation/UserDAO.java
@@ -0,0 +1,9 @@
+package edu.tamu.webtoxpi.dao.daoimplementation;
+
+import edu.tamu.webtoxpi.dao.daointefface.IUserDAO;
+import edu.tamu.webtoxpi.dao.entity.Users;
+import edu.tamu.webtoxpi.dao.util.GenericDAOImpl;
+
+public class UserDAO extends GenericDAOImpl<Users, Integer> implements IUserDAO
+{
+}
---
+++ src/main/java/edu/tamu/webtoxpi/dao/util/GenericDAO.java
@@ -0,0 +1,22 @@
+package edu.tamu.webtoxpi.dao.util;
+
+import java.io.*;
+import java.util.*;
+import org.hibernate.Query;
+ 
+public interface GenericDAO<T, ID extends Serializable> {
+ 
+    public void save(T entity);
+ 
+    public void merge(T entity);
+ 
+    public void delete(T entity);
+ 
+    public List<T> findMany(Query query);
+ 
+    public T findOne(Query query);
+ 
+    public List findAll(Class clazz);
+ 
+    public T findByID(Class clazz, Integer id);
+}
